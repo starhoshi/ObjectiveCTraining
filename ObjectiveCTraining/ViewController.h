98d0dbf949e472544034975e0da6c89c7a05f04c
star__hoshi <kensuke1751@gmail.com>
2016-09-15 15:06:56 +0900

    Initial Commit

---
+++ ObjectiveCTraining/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveCTraining
+//
+//  Created by Kensuke Hoshikawa on 2016/09/15.
+//  Copyright © 2016年 star__hoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjectiveCTraining/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ObjectiveCTraining
+//
+//  Created by Kensuke Hoshikawa on 2016/09/15.
+//  Copyright © 2016年 star__hoshi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
